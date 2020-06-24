aba3e0a1b32c8b181599bf4a54e7cb6f262f92c6
xeusteerapat <xeus085@gmail.com>
2020-06-24 14:42:06 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const uploadImage = async e => {
+    console.log('File uploading');
+    const { files } = e.target;
+
+    console.log(e.target.files);
+
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+    setLoading(true);
+
+    console.log(data);
+
+    for (let d of data.entries()) {
+      console.log(d[0] + ', ' + d[1]);
+    }
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/xeusteerapat/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    const fileImage = await res.json();
+    setImage(fileImage.secure_url);
+    setLoading(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Upload your image</h1>
+      <form action=''>
+        <input type='file' name='file' onChange={uploadImage} />
+        <button>Add</button>
+      </form>
+      <div className='preview'>
+        {loading ? <p>Loading...</p> : <img src={image} alt='pic' />}
+      </div>
     </div>
   );
 }
